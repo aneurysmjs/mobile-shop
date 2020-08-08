a6b3a902c04816296f1d43a3099699e5e5dedc40
jero <aneurysmjs@outlook.com>
2020-08-08 20:24:39 +0300

    chore(*): initial setup

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import styles from './Home.styles';
+
+export default function Home() {
+  return (
+    <View style={styles.homeContainer}>
+      <Text>Home</Text>
+    </View>
+  );
+}
