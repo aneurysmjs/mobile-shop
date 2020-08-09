c6da88d5d4383d80be0cf04108c96343fcf5c63e
jero <aneurysmjs@outlook.com>
2020-08-09 22:13:02 +0300

    chore(*): use typescript
    rename all files from .js to .tsx and create tsconfig

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
+import SearchInput from '../../components/SearchInput';
+
 import styles from './Home.styles';
 
 export default function Home() {
   return (
     <View style={styles.homeContainer}>
       <Text>Home</Text>
+      <SearchInput />
     </View>
   );
 }
