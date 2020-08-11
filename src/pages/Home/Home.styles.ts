a68b21c3c5a902008f26f5a25a59ab495c08d559
jero <aneurysmjs@outlook.com>
2020-08-11 23:17:50 +0300

    feat(home): use <Categories />

--- src/pages/Home/Home.styles.ts
+++ src/pages/Home/Home.styles.ts
@@ -1,9 +1,17 @@
 import { StyleSheet } from 'react-native';
 
+import layout from '@styles/layout';
+
 export default StyleSheet.create({
   homeContainer: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    ...layout.container,
+    marginTop: 80,
+    justifyContent: 'flex-start',
+  },
+  mainHeader: {
+    color: '#000',
+    fontSize: 30,
+    textAlign: 'center',
+    marginBottom: 20,
   },
 });
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, SafeAreaView } from 'react-native';
 
 import SearchInput from '@components/SearchInput';
+import Categories from '@components/Categories';
 
 import styles from './Home.styles';
 
 export default function Home(): React.ReactElement {
   return (
     <View style={styles.homeContainer}>
-      <Text>Home</Text>
-      <SearchInput placeHolder="Search Product" />
+      <SafeAreaView>
+        <Text style={styles.mainHeader}>Mobile Shop</Text>
+        <SearchInput placeHolder="Search Product" />
+        <Categories />
+      </SafeAreaView>
     </View>
   );
 }
