63a2a91c18d46165505f21c197f5b43ef116abdd
jero <aneurysmjs@outlook.com>
2020-08-11 23:15:26 +0300

    style(searchinput.styles): use variables

--- src/components/SearchInput/SearchInput.styles.ts
+++ src/components/SearchInput/SearchInput.styles.ts
@@ -1,13 +1,14 @@
 import { StyleSheet } from 'react-native';
 
 import padding from '@styles/utils/padding';
+import { BORDER_COLOR, BORDER_RADIUS, BORDER_WIDTH } from '@styles/variables';
 
 export default StyleSheet.create({
   input: {
     height: 40,
-    borderRadius: 5,
-    borderColor: 'gray',
-    borderWidth: 1,
+    borderRadius: BORDER_RADIUS,
+    borderColor: BORDER_COLOR,
+    borderWidth: BORDER_WIDTH,
     width: '100%',
     ...padding(0, 5),
   },
